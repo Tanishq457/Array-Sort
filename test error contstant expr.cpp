47bc6517f894f309c99527c1ec257fec48eaf0c0
Prateek Kr. Gupta <30932967+prateek45@users.noreply.github.com>
2018-10-04 23:28:25 +0530

    Simplified and Detailed the code
    Proper indentation and detail of each process

--- test error contstant expr.cpp
+++ test error contstant expr.cpp
@@ -4,24 +4,24 @@ int main()
 {
 	int n, i, j, c;
 	scanf("%d", &n);
+	
+	//Initializing array of size n
 	int arr[n], sort[n];
 	
+	// Enterning elements in array
 	for(i = 0; i<=n-1; i++)
 	{
 		scanf("%d", &arr[i]);
 	}
 	
+	//Sorting Array
 	for(j = 0; j < n; j++)
 	{
-	
 		int min = arr[0], max = arr[0];
-	
 		for(i = 0; i < n; i++)
 		{
 			if(arr[i] > max)
-			{
-				max = arr[i];	// MAX value
-			}
+			  max = arr[i];	// MAX value
 		}
 		
 	
@@ -29,26 +29,22 @@ int main()
 		{
 			if(arr[i] < min)
 			{
-				min = arr[i];	// MIN value
-				c = i;
+			   min = arr[i];	// MIN value
+			     c = i;
 			}
 		}
-		
-		
-		sort[j] =  min;
+		sort[j] = min;
 		arr[c] = max;
 			
 	}
-	
+	//Printing sorted Array (Ascending)
 	printf("Ascending order:\n\n");
-
 	for(i = 0; i<n; i++)
 	{
 		printf("%d\t", sort[i]);
 	}     
-
-	printf("Ascending order:\n\n");
-
+        //Printing sorted Array (Decending)
+	printf("Decending order:\n\n");
 	for(i = 0; i<n; i++)
 	{
 		printf("%d\t", sort[n-i-1]);
